12dd56378d4f40cd37fa30266b5cc4942983ac30
Imam Bux <imambuxpk@gmail.com>
2021-12-06 19:45:30 +0100

    Use https instead of SSH protocol

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('git@github.com:imambux/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/imambux/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('imambuxpk@gmail.com')
         }
