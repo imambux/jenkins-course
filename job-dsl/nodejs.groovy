d1906cfcfc1e2965ad5e3fd864a118f5906e5ab8
Imam Bux <imambuxpk@gmail.com>
2021-12-06 20:05:03 +0100

    Use right git repo

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/imambux/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/imambux/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('imambuxpk@gmail.com')
         }
