5af5b6eb469522ae3840a708c49bf1d8a6f2da7c
Imam Bux <imambuxpk@gmail.com>
2021-12-06 18:59:16 +0100

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git@github.com:imambux/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('imambuxpk@gmail.com')
         }
     }
     triggers {
